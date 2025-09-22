60b29b03efdade987cfb00b63998134516b22fa9
aleblbl <alell8632@gmail.com>
2025-09-22 18:21:46 +0200

    CMakeLists.txt fixes

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+
+// intellisense definitions
+#include "xbox_intellisense_platform.h"
+
+#include <stdio.h>
+#include <xtl.h>
+
+
+int main() {
+	//Log("Hello, World!\n");
+	OutputDebugString("H23HdaddassasdsAHAHA");
+	printf("CIAO %d\n", 2);
+	return 5355;
+}
