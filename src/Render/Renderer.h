80135be7080424520e1e70c34c309dda7f384d51
aleblbl <alell8632@gmail.com>
2025-09-23 02:14:37 +0200

    more `Renderer` fields and start of `Initialise` `CreateCommandBuffer` `SetupShaders`

--- src/Render/Renderer.h
+++ src/Render/Renderer.h
@@ -4,7 +4,25 @@
 
 struct Renderer
 {
+	D3DDevice* m_d3dDevice;
+	void* unk1;
+	void* unk2;
+	void* unk3;
+	void* unk4;
+	void* unk5;
+	void* unk6;
+	uint32_t unk7;
+	uint32_t unk8;
+	uint32_t criticalSection;
+	uint32_t gap6[6];
+
+	D3DPixelShader* pxShaders[3];
+	uint32_t unk_4c;
+	D3DVertexShader* vxShaders[3];
+
 	void Initialise(D3DDevice* d3dDevice);
+	void CreateCommandBuffer();
+	void SetupShaders();
 };
 
 
